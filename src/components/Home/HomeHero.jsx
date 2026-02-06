0a7fef771c8b8191e80f6093425d373828a0a8cb
Vikram Pal <vikrampal038@gmail.com>
2026-02-06 15:22:36 +0530

    complete Navbar responsive code

--- src/components/Home/HomeHero.jsx
+++ src/components/Home/HomeHero.jsx
@@ -14,7 +14,7 @@ const HomeHero = () => {
       </div>
 
       {/* Content */}
-      <div className="relative z-10 flex flex-col items-center gap-10 px-4">
+      <div className="relative z-10 flex flex-col items-center gap-10 px-4 border">
         {/* Text Content */}
         <div className=" w-full lg:w-4xl flex flex-col items-center gap-8 text-center">
           <div className="group flex items-center justify-center gap-5 rounded-2xl px-5 py-2 lg:w-150">
@@ -47,7 +47,7 @@ const HomeHero = () => {
 
           <NavLink
             to="/contact"
-            className="relative group overflow-hidden button border inline-flex items-center justify-center px-3 py-2  lg:px-6 lg:p lg:w-200yxl"
+            className="relative group overflow-hidden button border inline-flex items-center justify-center px-3 py-2  lg:px-6 lg:py-3"
           >
             <span className="absolute inset-0 bg-[#8be0ff5b] rounded-lg -translate-x-full group-hover:translate-x-0 transition-transform duration-700 ease-in-out" />
             <span className="relative z-10">Contact Us</span>
